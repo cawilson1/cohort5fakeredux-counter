75ed9e8610c17828f071513919cce328eba3c748
cawilson1 <cawilson1@g.cofc.edu>
2020-09-28 10:43:13 -0400

    switch useState to useReducer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./App.css";
 
+const reducer = (state, action) => {
+  if (action.type === "INCREMENT_COUNTER") {
+    return { ...state, counter: state.counter + 1 };
+  } else if (action.type === "DECREMENT_COUNTER") {
+    return { ...state, counter: state.counter - 1 };
+  }
+  return state;
+};
+
+const initialState = {
+  counter: 0
+};
+
 function App() {
-  const [count, setCount] = React.useState(0);
+  // const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
       <header className="App-header">
-        <div>{count}</div>
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <div>{state.counter}</div>
+        <button onClick={() => dispatch({ type: "INCREMENT_COUNTER" })}>
+          Increment
+        </button>
+        <button onClick={() => dispatch({ type: "DECREMENT_COUNTER" })}>
+          Decrement
+        </button>
       </header>
     </div>
   );
